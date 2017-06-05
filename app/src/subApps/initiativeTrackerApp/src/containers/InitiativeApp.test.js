d3299a417f698a2dcf6bdfa006585bb1091d63b7
DylanSharkey <Serghar12@gmail.com>
2017-06-05 13:31:52 -0700

    Added react-router menu functionality

--- app/src/subApps/initiativeTrackerApp/src/containers/InitiativeApp.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InitiativeApp from './InitiativeApp';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<InitiativeApp />, div);
-});
